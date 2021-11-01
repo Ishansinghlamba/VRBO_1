2ce03580c9314bbbc3c1aa1ebfb0a11873209975
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-02 02:47:01 +0530

    added icons

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import styles from "../Navbar/Navbar.module.css"
+function Navbar() {
+    return (
+        <div className={styles.main_nav}>
+            <img src="https://csvcus.homeaway.com/rsrcs/cdn-logos/5.8.0/sitename/vrbo/web/logo.svg" alt="logo"/>
+            <div>
+               <div>1</div>
+               <div>1</div>
+               <div>1</div>
+               <div>1</div>
+               <div>1</div>
+               <div>1</div>
+               <div>1</div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
