c66a5bbf1e3b5421d95567aef7e047b26b0f16f7
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-02 03:21:00 +0530

    new icons added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
-import styles from "../Navbar/Navbar.module.css"
+import styles from "../Navbar/Navbar.module.css";
+import { AiOutlineHeart } from 'react-icons/ai';
+import {MdOutlinePersonOutline} from "react-icons/md"
+import {IoPersonAddOutline} from "react-icons/io"
+
 function Navbar() {
     return (
         <div className={styles.main_nav}>
             <img src="https://csvcus.homeaway.com/rsrcs/cdn-logos/5.8.0/sitename/vrbo/web/logo.svg" alt="logo"/>
             <div>
-               <div>1</div>
-               <div>1</div>
+               <div className={styles.div1}><AiOutlineHeart/>
+               <span className={styles.dark}>Trip Boards</span>
+               </div>
+               <div className={styles.div1}><MdOutlinePersonOutline/>
+               <span className={styles.dark}>Log in</span>
+
+               </div>
                <div>1</div>
                <div>1</div>
                <div>1</div>
