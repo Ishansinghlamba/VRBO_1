e09e74ca0a83944bc8db0f5ca76cc001d30bfb3c
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-02 17:31:26 +0530

    made the main

--- src/Components/Section-1/Main.jsx
+++ src/Components/Section-1/Main.jsx
@@ -9,7 +9,21 @@ function Main() {
                      <div className={styles.fl1}>
                          <img src="https://odis.homeaway.com/odis/destination/a8f63526-cf00-4610-a6c4-810d1b12cd9f.hw2.jpg" alt="l1" />
                      </div>
-                     <div className={styles.fl2}></div>
+                     <div className={styles.fl2}>
+                         <div>
+                             <img src="https://odis.homeaway.com/odis/destination/7354b320-26d3-4fe7-8471-d940a15d9d43.hw2.jpg" alt="dog" />
+                         </div>
+                         <div>
+                             <img src="https://odis.homeaway.com/odis/homepage/2a55d131-f53a-455a-8737-45038b308798.hw2.jpg" alt="beach" />
+                         </div>
+                         <div>
+                             <img src="https://odis.homeaway.com/odis/destination/776c3818-9b31-4028-b702-fa1e70e6c13d.hw2.jpg" alt="home" />
+                         </div>
+                         <div>
+                             <img src="https://odis.homeaway.com/odis/destination/d7b69e76-921d-4abb-a5a3-acb2fba344de.hw2.jpg" alt="more than 3" />
+                         </div>
+
+                     </div>
 
                 </div>
             </div>
