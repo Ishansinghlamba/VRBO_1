19806f49b8bdef04e16d7f578ec7c9893abc0ea1
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-02 15:05:15 +0530

    added css effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Banner from "./Components/Banner/Banner";
 import Navbar from "./Components/Navbar/Navbar";
+import Main from "./Components/Section-1/Main";
 
 function App() {
   return (
     <div >
       <Navbar/>
       <Banner/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Components/Section-1/Main.jsx
@@ -0,0 +1,9 @@
+import styles from "../Section-1/Main.module.css";
+function Main() {
+    return (
+        <div className={styles.container}>
+        </div>
+    )
+}
+
+export default Main
