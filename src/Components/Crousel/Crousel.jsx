6d799d78eb33154f3981351cde9fe2ae368a62ee
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-03 14:32:04 +0530

    added data

--- src/Components/Crousel/Crousel.jsx
+++ src/Components/Crousel/Crousel.jsx
@@ -1,7 +1,17 @@
 import styles from "../Crousel/Crousel.module.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./crousel.css";
+import {data} from "./criusel_data"
+
 function Crousel() {
+    console.log(data);
     return (
         <div className={styles.cont}>
+            <Slider>
+            
+            </Slider>
         </div>
     )
 }
---
+++ src/Components/Crousel/criusel_data.js
@@ -0,0 +1,10 @@
+var img1="https://odis.homeaway.com/odis/destination/8621e10e-7950-41b4-814e-8817969887f4.carousel-m.jpg";
+var img2 = "https://odis.homeaway.com/odis/destination/397eabda-51d4-45d4-82c8-da3ad409ff84.carousel-m.jpg";
+var img3 = "https://odis.homeaway.com/odis/destination/5941b1e0-2600-4b2a-b27e-c667abf7e510.carousel-m.jpg";
+var img4 = "https://odis.homeaway.com/odis/destination/ae891341-b7bb-4ae2-a1f8-7c8e53d16c4e.carousel-m.jpg";
+var img5 = "https://odis.homeaway.com/odis/destination/ae891341-b7bb-4ae2-a1f8-7c8e53d16c4e.carousel-m.jpg";
+var img6 = "https://odis.homeaway.com/odis/destination/adf45333-140a-40cf-987f-bc49b711c3d9.carousel-m.jpg";
+var img7 = "https://odis.homeaway.com/odis/destination/9af508c6-3dd9-453f-9a0d-8726a705328d.carousel-m.jpg";
+var img8 = "https://odis.homeaway.com/odis/destination/b7cb1fe3-5876-48a3-b9e4-c9b24d9f96b1.carousel-m.jpg"
+const data = [img1,img2,img3,img4,img5,img6,img7,img8]
+export {data};
