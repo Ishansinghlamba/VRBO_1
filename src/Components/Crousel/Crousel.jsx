570a26bd58e3179391816017d8ae846c2df48bbc
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-03 15:51:43 +0530

    added crousel

--- src/Components/Crousel/Crousel.jsx
+++ src/Components/Crousel/Crousel.jsx
@@ -3,14 +3,34 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./crousel.css";
-import {data} from "./criusel_data"
+import {data} from "./criusel_data";
+import {IoIosArrowDropleft} from "react-icons/io";
+import {IoIosArrowDropright} from "react-icons/io"
 
+const Prev=(props)=>{
+    const {className,onClick} = props;
+    return(
+        <div className={className} onClick={onClick}>
+           <IoIosArrowDropleft size={45} className={styles.left}/> 
+        </div>
+    )
+}
+const Next=(props)=>{
+    const {className,onClick} = props;
+    return(
+        <div className={className} onClick={onClick}>
+           <IoIosArrowDropright size={45} className={styles.right} /> 
+        </div>
+    )
+}
 function Crousel() {
-    console.log(data);
     return (
         <div className={styles.cont}>
-            <Slider>
-            
+            <Slider prevArrow={<Prev/>} nextArrow={<Next/>}
+>
+              {data.map((i)=>(<div>
+                  <img src={i} alt="mn" style={{width:"100%",height:"400px"}}/>
+              </div>))}
             </Slider>
         </div>
     )
