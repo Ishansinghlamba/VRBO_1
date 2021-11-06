f7963c00157105698fb52672d58ea3e6206afc7d
Ishan Singh <ishan_nj2_014@masai.school>
2021-11-06 13:24:22 +0530

    added effects

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -7,7 +7,13 @@ function Modal({setOpen}) {
                     <h2 className={styles.cross} onClick={()=>{setOpen(false)}}>X</h2>
                     <h2 className={styles.text}>Log in or sign up on Vrbo</h2>
                 </div>
-                <div className={styles.inside_box}></div>
+                <div className={styles.inside_box}>
+                    {/* <input type="email" placeholder="Email Address" /> */}
+                    <div className={styles.inputField}>
+                 <input type="text" placeholder=" " />
+                  <span>Email address</span>
+                        </div>
+                </div>
             </div>
         </div>
     )
